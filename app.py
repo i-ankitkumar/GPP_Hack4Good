64f4e88b056cde62810e62a61d48df8ff16e0ab5
Pratik Chaudhari <36737722+Proxxc29@users.noreply.github.com>
2020-05-02 11:59:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ app = Flask(__name__)
 CORS(app)
 
 
-PATH_TO_FROZEN_GRAPH = "/graph/frozen_inference_graph.pb"
-PATH_TO_LABEL_MAP = "/graph/label_map.pbtxt"
-IMAGE_PATH = "/static/images"
+PATH_TO_FROZEN_GRAPH = "graph/frozen_inference_graph.pb"
+PATH_TO_LABEL_MAP = "graph/label_map.pbtxt"
+IMAGE_PATH = "static/images"
 NUM_CLASSES = 7
 
 #reads the frozen graph
@@ -111,10 +111,10 @@ def predict():
         img = base64_to_pil(request.json)
 
         # Save the image to ./uploads
-        img.save("/uploads/image.jpg")
+        img.save("uploads/image.jpg")
 
         # Make prediction
-        img = cv2.imread("/uploads/image.jpg")
+        img = cv2.imread("uploads/image.jpg")
         # print(img)
         img_path = model_predict(img)
         
